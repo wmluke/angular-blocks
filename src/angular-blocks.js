ba82ff1d4d279f5229aaaf7818cf9f165314b15d
Perry Roper <perryroper@gmail.com>
2014-02-03 10:40:35 +0000

    Remove jquery dependency. This unfortunately breaks IE7 support and data-block-before.

--- src/angular-blocks.js
+++ src/angular-blocks.js
@@ -15,48 +15,54 @@
                     throw 'Template not specified in extend-template directive';
                 }
                 // Clone and then clear the template element to prevent expressions from being evaluated
-                var $clone = tElement.clone();
+                var clone = angular.element(tElement.clone());
                 tElement.html('');
 
                 var loadTemplate = $http.get(src, {cache: $templateCache})
                     .then(function (response) {
-                        var template = response.data;
-                        var $template = $(document.createElement('div')).html(template);
+                        var template = angular.element(document.createElement('div')).html(response.data);
 
-                        function override(method, $block, attr) {
-                            var name = $block.attr(attr);
-                            if ($template.find('[data-block="' + name + '"]')[method]($block).length === 0 &&
-                                $template.find('[data-extend-template]').append($block).length === 0) {
+                        function override(method, block, attr) {
+                            var name = block[0].getAttribute(attr);
+
+                            if (angular.element(template[0].querySelectorAll('[data-block="' + name + '"]'))[method](block).length === 0 &&
+                                angular.element(template[0].querySelectorAll('[data-extend-template]')).append(block).length === 0) {
                                 warnMissingBlock(name, src);
                             }
                         }
 
-                        // Replace overridden blocks
-                        $clone.children('[data-block]').each(function () {
-                            override('replaceWith', $(this), 'data-block');
+                        angular.forEach(clone.children(), function (el) {
+                            var $el = angular.element(el);
+
+                            // Replace overridden blocks
+                            if (el.hasAttribute('data-block')) {
+                                override('replaceWith', $el, 'data-block');
+                            }
+
+                            // Insert prepend blocks
+                            if (el.hasAttribute('data-block-prepend')) {
+                                override('prepend', $el, 'data-block-prepend');
+                            }
+
+                            // Insert append blocks
+                            if (el.hasAttribute('data-block-append')) {
+                                override('append', $el, 'data-block-append');
+                            }
+
+                            // TODO: angular.element does not have a `.before`
+                            // method. Potentially monkey patch this.
+                            // Insert before blocks
+                            // if (el.hasAttribute('data-block-before')) {
+                            //     override('before', $el, 'data-block-before');
+                            // }
+
+                            // Insert after blocks
+                            if (el.hasAttribute('data-block-after')) {
+                                override('after', $el, 'data-block-after');
+                            }
                         });
 
-                        // Insert prepend blocks
-                        $clone.children('[data-block-prepend]').each(function () {
-                            override('prepend', $(this), 'data-block-prepend');
-                        });
-
-                        // Insert append blocks
-                        $clone.children('[data-block-append]').each(function () {
-                            override('append', $(this), 'data-block-append');
-                        });
-
-                        // Insert before blocks
-                        $clone.children('[data-block-before]').each(function () {
-                            override('before', $(this), 'data-block-before');
-                        });
-
-                        // Insert after blocks
-                        $clone.children('[data-block-after]').each(function () {
-                            override('after', $(this), 'data-block-after');
-                        });
-
-                        return $template;
+                        return template;
                     }, function () {
                         var msg = 'Failed to load template: ' + src;
                         $log.error(msg);
@@ -65,8 +71,8 @@
 
 
                 return function ($scope, $element) {
-                    loadTemplate.then(function ($template) {
-                        $element.html($template.html());
+                    loadTemplate.then(function (template) {
+                        $element.html(template.html());
                         $compile($element.contents())($scope);
                     });
                 };
