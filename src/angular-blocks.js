b3e32250bd0d572c5089b59662743a13fcbe8e7c
Perry Roper <perryroper@gmail.com>
2014-02-03 15:24:33 +0000

    Add functionality to replace override blocks

--- src/angular-blocks.js
+++ src/angular-blocks.js
@@ -22,8 +22,10 @@
                     .then(function (response) {
                         var template = angular.element(document.createElement('div')).html(response.data);
 
-                        function override(method, block, attr) {
+                        function override(method, block, attr, replace) {
                             var name = block[0].getAttribute(attr);
+
+                            block = replace ? block.html() : block;
 
                             if (angular.element(template[0].querySelectorAll('[data-block="' + name + '"]'))[method](block).length === 0 &&
                                 angular.element(template[0].querySelectorAll('[data-extend-template]')).append(block).length === 0) {
@@ -34,31 +36,36 @@
                         angular.forEach(clone.children(), function (el) {
                             var $el = angular.element(el);
 
+                            var replace = false;
+                            if (el.hasAttribute('replace')) {
+                                replace = true;
+                            }
+
                             // Replace overridden blocks
                             if (el.hasAttribute('data-block')) {
-                                override('replaceWith', $el, 'data-block');
+                                override('replaceWith', $el, 'data-block', replace);
                             }
 
                             // Insert prepend blocks
                             if (el.hasAttribute('data-block-prepend')) {
-                                override('prepend', $el, 'data-block-prepend');
+                                override('prepend', $el, 'data-block-prepend', replace);
                             }
 
                             // Insert append blocks
                             if (el.hasAttribute('data-block-append')) {
-                                override('append', $el, 'data-block-append');
+                                override('append', $el, 'data-block-append', replace);
                             }
 
                             // TODO: angular.element does not have a `.before`
                             // method. Potentially monkey patch this.
                             // Insert before blocks
                             // if (el.hasAttribute('data-block-before')) {
-                            //     override('before', $el, 'data-block-before');
+                            //     override('before', $el, 'data-block-before', replace);
                             // }
 
                             // Insert after blocks
                             if (el.hasAttribute('data-block-after')) {
-                                override('after', $el, 'data-block-after');
+                                override('after', $el, 'data-block-after', replace);
                             }
                         });
 
